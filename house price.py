9aefaeb534a940a1a2c9d112781e0153849f4561
saquibvai <saquib.archo@gmail.com>
2019-08-03 22:52:01 +0600

    First Commit

--- house price.py
+++ house price.py
@@ -1,58 +1,14 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-%matplotlib inline
-import matplotlib.pyplot as plt  # Matlab-style plotting
+import pandas as pd
+import matplotlib.pyplot as plt
 import seaborn as sns
-color = sns.color_palette()
-sns.set_style('darkgrid')
-import warnings
-def ignore_warn(*args, **kwargs):
-    pass
-warnings.warn = ignore_warn #ignore annoying warning (from sklearn and seaborn)
-
-
+import numpy as np
+from scipy.stats import norm
+from sklearn.preprocessing import StandardScaler
 from scipy import stats
-from scipy.stats import norm, skew #for some statistics
-
-
-pd.set_option('display.float_format', lambda x: '{:.3f}'.format(x)) #Limiting floats output to 3 decimal points
-
-
-from subprocess import check_output
-print(check_output(["ls", "../input"]).decode("utf8"))
-
-train = pd.read_csv('../input/train.csv')
-test = pd.read_csv('../input/test.csv')
-##display the first five rows of the train dataset.
-train.head(5)
-
-test.head(5)
-print("The train data size before dropping Id feature is : {} ".format(train.shape))
-print("The test data size before dropping Id feature is : {} ".format(test.shape))
-
-#Save the 'Id' column
-train_ID = train['Id']
-test_ID = test['Id']
-
-#Now drop the  'Id' colum since it's unnecessary for  the prediction process.
-train.drop("Id", axis = 1, inplace = True)
-test.drop("Id", axis = 1, inplace = True)
-
-#check again the data size after dropping the 'Id' variable
-print("\nThe train data size after dropping Id feature is : {} ".format(train.shape)) 
-print("The test data size after dropping Id feature is : {} ".format(test.shape))
-
-fig, ax = plt.subplots()
-ax.scatter(x = train['GrLivArea'], y = train['SalePrice'])
-plt.ylabel('SalePrice', fontsize=13)
-plt.xlabel('GrLivArea', fontsize=13)
-plt.show()
-
-train = train.drop(train[(train['GrLivArea']>4000) & (train['SalePrice']<300000)].index)
-
-#Check the graphic again
-fig, ax = plt.subplots()
-ax.scatter(train['GrLivArea'], train['SalePrice'])
-plt.ylabel('SalePrice', fontsize=13)
-plt.xlabel('GrLivArea', fontsize=13)
-plt.show()
+import warnings
+warnings.filterwarnings('ignore')
+%matplotlib inline
+df_train = pd.read_csv('../input/train.csv')
+#check the decoration
+df_train.columns
+df_train['SalePrice'].describe()
